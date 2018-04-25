0cd6bd97a17e49febf7a0f5038753a64edcbd458
sel006 <githubemail006@gmail.com>
2018-04-25 12:18:04 -0500

    Improved Performance and simplified code

---
+++ Selection.py
@@ -0,0 +1,86 @@
+def tall(pictotal, page8_count, last_page8_size, page):
+    offset = 2
+    wmax = 1
+    hmax = 3
+    WMAX = 1
+    HMAX = 3
+    maxnum = 0
+    pic = []
+    if len(pictotal) <= 8:
+        pic = pictotal
+    else:
+        if page == page8_count:
+           for x in range(0, last_page8_size):
+                pic.append(pictotal[x+(page*8)])
+        else:
+           for x in range(0, 8):
+                pic.append(pictotal[x+(page*8)])
+
+    maxnum = len(pic)
+    tmaxnum = maxnum - 1
+    flag = 0
+    for x in range(0, hmax+1):
+        if not flag:
+            if tmaxnum <= (x*offset)+wmax:
+                hmax = x
+                flag = 1
+    flag = 0
+    for x in range(0, HMAX+1):
+        if not flag:
+            if tmaxnum <= (x*offset)+WMAX:
+                wmax = tmaxnum-(x*offset)
+                flag = 1
+    if maxnum < 2:
+        WMAX = wmax
+
+    vals = []
+    vals.append(wmax)
+    vals.append(hmax)
+    vals.append(WMAX)
+    vals.append(HMAX)
+    vals.append(maxnum)
+    vals.append(pic)
+    return vals
+
+def wide(pictotal, page12_count, last_page12_size, page):
+    wmax = 3
+    hmax = 2
+    WMAX = 3
+    HMAX = 2
+    offset = 4
+    pic = []
+    if len(pictotal) <= 12:
+        pic = pictotal
+    else:
+        if page == page12_count:
+            for x in range(0, last_page12_size):
+                pic.append(pictotal[x+(page*12)])
+        else:
+            for x in range(0,12):
+                pic.append(pictotal[x+(page*12)])
+
+    maxnum = len(pic)
+    tmaxnum = maxnum - 1
+    flag = 0
+    for x in range(0, hmax):
+        if not flag:
+            if tmaxnum <= (x*offset)+wmax:
+                hmax = x
+                flag = 1
+    flag = 0
+    for x in range(0, HMAX):
+        if not flag:
+            if tmaxnum <= (x*offset)+WMAX:
+                wmax = tmaxnum-(x*offset)
+                flag = 1
+    if maxnum < 4:
+        WMAX = wmax
+
+    vals = []
+    vals.append(wmax)
+    vals.append(hmax)
+    vals.append(WMAX)
+    vals.append(HMAX)
+    vals.append(maxnum)
+    vals.append(pic)
+    return vals
--- img.py
+++ img.py
@@ -5,6 +5,7 @@ import sys
 import Image
 import Window
 import Misc
+import Selection
 
 #Initializes ncurses related settings
 def initwin():
@@ -21,7 +22,10 @@ def endall():
     curses.echo()
     curses.endwin()
 
-pictures = filter(lambda x: x.endswith('.jpg') or x.endswith('.png'), os.listdir('/path/to/folder'))
+cwd = os.getcwd()
+
+#pictures = filter(lambda x: x.endswith('.jpg') or x.endswith('.png'), os.listdir('/home/dave/Pictures/testdir'))
+pictures = filter(lambda x: x.endswith('.jpg') or x.endswith('.png'), os.listdir(cwd))
 pictotal = []
 for x in pictures:
     pictotal.append(x)
@@ -44,6 +48,7 @@ prevy = 0
 prevx = 0
 
 page = 0
+prevpage = page
 
 if len(pictotal) > 8:
     page8_count = int(len(pictotal)/8)
@@ -53,92 +58,55 @@ if len(pictotal) > 12:
     page12_count = int(len(pictotal)/12)
     last_page12_size = len(pictotal)-(page12_count*12)
 
+height,width = stdscr.getmaxyx()
+
+if (2*height)>width:
+    dir = 'k'
+else:
+    dir = 'k'
+
+resetflag = 0
 
 while 1:
 
     stdscr.refresh()
 
     height,width = stdscr.getmaxyx()
-    pic = []
-    maxnum = 0
+
+    prev_dir = dir
+
+    Sflag = 0
 
     if (2*height)>width:
-        offset = 2
-        wmax = 1
-        hmax = 3
-        WMAX = 1
-        HMAX = 3
-        if len(pictotal) <= 8:
-            pic = pictotal
-            maxnum = len(pic)
-        else:
-            if page == page8_count:
-                for x in range(0, last_page8_size):
-                    pic.append(pictotal[x+(page*8)])
-                maxnum = len(pic)
-            else:
-                for x in range(0, 8):
-                    pic.append(pictotal[x+(page*8)])
-                maxnum = len(pic)
-        if 1:
-            tmaxnum = maxnum - 1
-            flag = 0
-            for x in range(0, hmax+1):
-                if not flag:
-                    if tmaxnum <= (x*offset)+wmax:
-                        hmax = x
-                        flag = 1
-            flag = 0
-            for x in range(0, HMAX+1):
-                if not flag:
-                    if tmaxnum <= (x*offset)+WMAX:
-                        wmax = tmaxnum-(x*offset)
-                        flag = 1
-            if maxnum < 2:
-                WMAX = wmax
-        else:
-            for x in range(page, page+8):
-                pic.append(pictotal[x])
-            maxnum = 8
-
+        dir = 'l'
     else:
-        wmax = 3
-        hmax = 2
-        WMAX = 3
-        HMAX = 2
-        offset = 4
-        if len(pictotal) <= 12:
-            pic = pictotal
-            maxnum = len(pic)
+        dir = 'w'
+
+    if dir != prev_dir or prevpage != page:
+        if prevpage != page:
+            stdscr.clear()
+            stdscr.refresh()
+        pic = []
+        maxnum = 0
+        if dir == 'l':
+            offset = 2
+            vals = Selection.tall(pictotal, page8_count, last_page8_size, page)
+            wmax = vals[0]
+            hmax = vals[1]
+            WMAX = vals[2]
+            HMAX = vals[3]
+            maxnum = vals[4]
+            pic = vals[5]
         else:
-            if page == page12_count:
-                for x in range(0, last_page12_size):
-                    pic.append(pictotal[x+(page*12)])
-                maxnum = len(pic)
-            else:
-                for x in range(0, 12):
-                    pic.append(pictotal[x+(page*12)])
-                maxnum = len(pic)
-        if 1:
-            tmaxnum = maxnum - 1
-            flag = 0
-            for x in range(0, hmax):
-                if not flag:
-                     if tmaxnum <= (x*offset)+wmax:
-                         hmax = x
-                         flag = 1
-            flag = 0
-            for x in range(0, HMAX):
-                if not flag:
-                    if tmaxnum <= (x*offset)+WMAX:
-                        wmax = tmaxnum-(x*offset)
-                        flag = 1
-            if maxnum < 4:
-                WMAX = wmax
-        else:
-            for x in range(page, page+12):
-                pic.append(pictotal[x])
-            maxnum = 8
+            offset = 4
+            vals = Selection.wide(pictotal, page12_count, last_page12_size, page)
+            wmax = vals[0]
+            hmax = vals[1]
+            WMAX = vals[2]
+            HMAX = vals[3]
+            maxnum = vals[4]
+            pic = vals[5]
+        Sflag = 1
 
     cursx += Misc.check_limitx(cursx, cursy, hmax, wmax, prevy, WMAX)
     cursy += Misc.check_limity(cursx, cursy, hmax, wmax, prevx)
@@ -146,10 +114,13 @@ while 1:
     index = cursy * offset + cursx
 
     wins = Window.init_windows(height, width, index, maxnum)
-    ims = Image.init_images(pic, height, width)
-
     Window.draw_windows(wins)
-    Image.draw_images(ims)
+    if Sflag or resetflag:
+        ims = Image.init_images(pic, height, width)
+        Image.draw_images(ims)
+        resetflag = 0
+
+    prevpage = page
 
     c = stdscr.getch()
     if c == ord('q'):
@@ -164,15 +135,18 @@ while 1:
             d = stdscr.getch()
             if d == ord('f'):
                 stdscr.clear()
+                resetflag = 1
                 break
     if c == ord('p') and page > 0:
         page -= 1
 
     if (2*height)>width:
         if c == ord('n') and page < page8_count:
+            prevpage = page
             page += 1
     else:
         if c == ord('n') and page < page12_count:
+            prevpage = page
             page += 1
 
     prevx = cursx
@@ -182,4 +156,3 @@ while 1:
     cursy += Misc.inputy(c)
         
     Window.draw_windows(wins)
-#    stdscr.clear()
